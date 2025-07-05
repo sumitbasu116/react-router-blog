67d0bb2c6f9694e2574850a33f926f45645435ff
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 23:38:37 +0530

    basic route part2-basic routes with Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { Route } from "react-router"
+import { Link, Route } from "react-router"
 import { Routes } from "react-router"
 
 function App() {
 
   return (
     <>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/login">Login</Link>
+
     <Routes>
       <Route path="/" element={<h1>Home Page</h1>}/>
       <Route path="/about" element={<h1>About Page</h1>}/>
+      <Route path="/login" element={<h1>Login Page</h1>}/>
     </Routes>
     </>
   )
