e80ed30b09685a53e07258c1d8bce8fdc729a6a5
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 23:34:13 +0530

    basic route part1-basic example of route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { Route } from "react-router"
 import { Routes } from "react-router"
-import Home from "./Home"
-import About from "./About"
-import { Link } from "react-router"
 
 function App() {
 
   return (
     <>
-      <Link to="/">Home Page</Link>
-      <Link to="/about">About Page</Link>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
-      
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>}/>
+      <Route path="/about" element={<h1>About Page</h1>}/>
+    </Routes>
     </>
   )
 }
-
 export default App
