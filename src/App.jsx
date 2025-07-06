e17565a45b968c6642eb7a7dd46b79db4ecaa7bd
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-06 10:06:29 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import {Route } from "react-router"
-import { Routes } from "react-router"
-import NavBar from "./NavBar"
 
 function App() {
 
   return (
     <>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>}/>
-      <Route path="/about" element={<h1>About Page</h1>}/>
-      <Route path="/login" element={<h1>Login Page</h1>}/>
-    </Routes>
+    
     </>
   )
 }
